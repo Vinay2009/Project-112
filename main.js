52af3c6566ac2bde308b389f8ac00b665c1f4cdd
Vinay2009 <91785104+Vinay2009@users.noreply.github.com>
2022-06-06 18:03:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+https://teachablemachine.withgoogle.com/models/XYdiNW2R6/model.json
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+    });
+    
+    camera = document.getElementById("camera");
+    
+    Webcam.attach('#camera');
+    
+    function take_snapshot()
+    {
+        Webcam.snap(function(data_uri) {
+            document.getElementById("result").innerHTML = '<img id="captured_image" src="' +data_uri+'"/>';
+        });
+    }
+    console.log('ml5 version:', ml5.version);
+    
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/XYdiNW2R6/model.json',)
+    
+    function modelLoaded() {
+        console.log('Model Loaded!');
+    }
+    
+    function speak(){
+        var synth = window.speechSynthesis;
+        speak_data_1 = "The first prediciton is" + prediction_1;
+        speak_data_2 = "The second prediciton is" + prediction_2;
+        var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+        synth.speak(utterThis);
+    }
+
+    function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML = results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+    if(results[0].label == "Super")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#1F44C;";
+    }
+    if(results[0].label == "What")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#1F596;";
+    }
+    if(results[0].label == "Thumbs Up")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#1F44D;";
+    }
+    if(results[0].label == "Thumbs Down")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#1F44E;";
+    }
+
+    if(results[0].label == "Super")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#1F44C;";
+    }
+    if(results[0].label == "What")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#1F596;";
+    }
+    if(results[0].label == "Thumbs Up")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#1F44D;";
+    }
+    if(results[0].label == "Thumbs Down")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#1F44E;";
+    }
+}
+}
